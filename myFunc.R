7fcc3b2ad998a42f71ae6e9e347d1e9633956081
RajuProbStat <139469223+RajuProbStat@users.noreply.github.com>
2023-08-18 22:04:52 +0530

    Add files via upload

---
+++ myFunc.R
@@ -0,0 +1,14 @@
+#' Title
+#'
+#' @param a the first number
+#' @param b the second number
+#'
+#' @return sum
+#' @export
+#'
+#' @examples
+#' myFunc(2,3)
+myFunc<-function(a,b)
+{
+  return(a+b)
+}
